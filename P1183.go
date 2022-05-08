e2aa267cfb9c750d6aa150bfcd7f3ae357b39df7
InSun <aolgit@aol.com>
2022-05-09 00:19:43 +0900

    P1183/Go

---
+++ P1183.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+var br *bufio.Scanner = bufio.NewScanner(os.Stdin)
+var bw *bufio.Writer = bufio.NewWriter(os.Stdout)
+
+func main() {
+
+	br.Scan()
+	N, _ := strconv.Atoi(br.Text())
+
+	ints := make([]int, N)
+	for i := 0; i < N; i++ {
+		br.Scan()
+		slice := strings.Split(br.Text(), " ")
+		A, _ := strconv.Atoi(slice[0])
+		B, _ := strconv.Atoi(slice[1])
+
+		ints[i] = A - B
+	}
+
+	sort.Slice(ints, func(i, j int) bool {
+		return ints[i] < ints[j]
+	})
+
+	if N%2 == 1 {
+		fmt.Println(1)
+	} else {
+		fmt.Println(ints[N/2] - ints[(N/2)-1] + 1)
+	}
+}
