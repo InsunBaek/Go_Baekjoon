0a3cdee5fc9d399ead992c44912be2ebd8a8c12d
InSun <aolgit@aol.com>
2022-06-04 00:43:26 +0900

    P25083/Go

---
+++ P25083.go
@@ -0,0 +1,24 @@
+// Print
+// https://www.acmicpc.net/problem/25083
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+var br *bufio.Scanner = bufio.NewScanner(os.Stdin)
+var bw *bufio.Writer = bufio.NewWriter(os.Stdout)
+
+func main() {
+
+	fmt.Println("         ,r'\"7")
+	fmt.Println("r`-_   ,'  ,/")
+	fmt.Println(" \\. \". L_r'")
+	fmt.Println("   `~\\/")
+	fmt.Println("      |")
+	fmt.Println("      |")
+
+}
