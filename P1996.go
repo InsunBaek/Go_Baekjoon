706ad0a12e6916cb0d84848c1875d7f420578171
InSun <aolgit@aol.com>
2022-07-08 23:55:04 +0900

    P1996/Go

---
+++ P1996.go
@@ -0,0 +1,78 @@
+// Implementation
+// https://www.acmicpc.net/problem/1996
+
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+var bw = bufio.NewWriter(os.Stdout)
+
+func Fn() {
+	N := scanInt()
+	graph := make([][]string, N)
+	makedG := make([][]int, N)
+
+	for i := 0; i < N; i++ {
+		graph[i] = make([]string, N)
+		makedG[i] = make([]int, N)
+	}
+
+	for i := 0; i < N; i++ {
+		sc.Scan()
+		line := sc.Text()
+		for j := 0; j < N; j++ {
+			graph[i][j] = string(line[j])
+		}
+	}
+
+	for i := 0; i < N; i++ {
+		for j := 0; j < N; j++ {
+			if graph[i][j] != "." {
+				for k := 0; k < 8; k++ {
+					nRow := i + dy[k]
+					nCol := j + dx[k]
+					if canVisit(N, nRow, nCol) && graph[nRow][nCol] == "." {
+						value, _ := strconv.Atoi(graph[i][j])
+						makedG[nRow][nCol] += value
+						if makedG[nRow][nCol] >= 10 {
+							makedG[nRow][nCol] = math.MaxInt16
+						}
+					}
+				}
+				makedG[i][j] = math.MaxInt32
+			}
+		}
+	}
+
+	for i := 0; i < N; i++ {
+		for j := 0; j < N; j++ {
+			if makedG[i][j] == math.MaxInt32 {
+				fmt.Fprint(bw, "*")
+			} else if makedG[i][j] == math.MaxInt16 {
+				fmt.Fprint(bw, "M")
+			} else {
+				fmt.Fprint(bw, makedG[i][j])
+			}
+		}
+		fmt.Fprintln(bw)
+	}
+}
+
+func main() {
+	defer bw.Flush()
+
+	Fn()
+}
+
+func scanInt() int {
+	sc.Scan()
+	v, _ := strconv.Atoi(sc.Text())
+	return v
+}
